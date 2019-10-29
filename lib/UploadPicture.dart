a011591e4bed10689b3b6d08a4c17c00ffccb9da
Adrien Deguffroy <adrien-deg@hotmail.fr>
2019-10-29 12:46:23 +0100

    User can change is Profile Picture (Same update as before, a file was missing)
    When user change is PP, a batched write is started to ensure that every operations can be done, if something wrong happen nothing will be write
    
    Add :
    Image picker library to select picture from gallery (Camera will be added later)
    UUID library to generate random name for file uploaded
    
    Change :
    Start replacing a lot of values by a const in Constants file

---
+++ lib/UploadPicture.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
+
+abstract class BaseUpload {
+  Future<String> uploadProfilePic(String userId, File imageFile);
+}
+
+class Upload extends BaseUpload {
+
+  @override
+  Future<String> uploadProfilePic(String userId, File imageFile) async {
+    var uuid = Uuid();
+    StorageReference storageReference = FirebaseStorage.instance.ref().child("images/$userId/${uuid.v1()}.jpg");
+    StorageUploadTask storageUploadTask = storageReference.putFile(imageFile);
+    final StorageTaskSnapshot downloadUrl =
+    (await storageUploadTask.onComplete);
+    final String url = (await downloadUrl.ref.getDownloadURL());
+    return url;
+  }
+}
